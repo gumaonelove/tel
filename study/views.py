c18450b126139c269803c940e47e8592ba2a141b
gumaonelove <68272600+gumaonelove@users.noreply.github.com>
2022-04-16 12:57:32 +0300

    правки

--- study/views.py
+++ study/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 
-from .models import Student
+from .models import Student, Texts
 
 
 class LearnView(View):
@@ -20,12 +20,35 @@ class ReadingView(View):
     '''main page view'''
     def get(self, request):
         if request.user.is_authenticated:
+            student = Student.objects.get(user=request.user)
+            student_texts = student.texts.all()
+            texts = Texts.objects.all()
+            king_of_the_reading = False
+
+            not_reading_texts = []
+
+            for text in texts:
+                if text not in student_texts:
+                    not_reading_texts.append(text)
+
+            if not_reading_texts:
+                text = not_reading_texts[0]
+            else:
+                king_of_the_reading = True
+                text = texts.first()
+
             context = {
-                'student': Student.objects.get(user=request.user)
+                'student': student,
+                'text': text,
+                'king_of_the_reading': king_of_the_reading
             }
             return render(request, 'reading.html', context)
         else:
             return redirect('/')
+
+
+    def post(self, request):
+        print(request)
 
 
 class AuditionView(View):
