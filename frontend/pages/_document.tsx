853d08a45f274bcd8fa0f1710486d948515cb482
Ildar <ildarvaliev167@gmail.com>
2023-02-06 23:57:46 +0300

    добавил фавиконку

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -14,80 +14,81 @@ export default class MyDocument extends Document {
           <meta name="format-detection" content="telephone=no"/>
           <meta property="og:site_name" content={"TelSafTatar"} key={"ogsitename"}/>
           <meta property="og:locale" content={"ru"} key={"oglocale"}/>
-          {/*<link rel="shortcut icon" href="/img/favicon/favicon.ico"/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="57x57"*/}
-          {/*  href="/img/favicon/apple-icon-57x57.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="60x60"*/}
-          {/*  href="/img/favicon/apple-icon-60x60.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="72x72"*/}
-          {/*  href="/img/favicon/apple-icon-72x72.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="76x76"*/}
-          {/*  href="/img/favicon/apple-icon-76x76.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="114x114"*/}
-          {/*  href="/img/favicon/apple-icon-114x114.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="120x120"*/}
-          {/*  href="/img/favicon/apple-icon-120x120.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="144x144"*/}
-          {/*  href="/img/favicon/apple-icon-144x144.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="152x152"*/}
-          {/*  href="/img/favicon/apple-icon-152x152.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="apple-touch-icon"*/}
-          {/*  sizes="180x180"*/}
-          {/*  href="/img/favicon/apple-icon-180x180.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="icon"*/}
-          {/*  type="image/png"*/}
-          {/*  sizes="192x192"*/}
-          {/*  href="/img/favicon/android-icon-192x192.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="icon"*/}
-          {/*  type="image/png"*/}
-          {/*  sizes="32x32"*/}
-          {/*  href="/img/favicon/favicon-32x32.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="icon"*/}
-          {/*  type="image/png"*/}
-          {/*  sizes="96x96"*/}
-          {/*  href="/img/favicon/favicon-96x96.png"*/}
-          {/*/>*/}
-          {/*<link*/}
-          {/*  rel="icon"*/}
-          {/*  type="image/png"*/}
-          {/*  sizes="16x16"*/}
-          {/*  href="/img/favicon/favicon-16x16.png"*/}
-          {/*/>*/}
-          {/*<meta*/}
-          {/*  name="msapplication-TileImage"*/}
-          {/*  content="/img/favicon/ms-icon-144x144.png"*/}
-          {/*></meta>*/}
+          <link rel="icon" href="/icons/favicon/favicon.svg"/>
+          <link rel="shortcut icon" href="/icons/favicon/favicon.ico"/>
+          <link
+            rel="apple-touch-icon"
+            sizes="57x57"
+            href="/icons/favicon/apple-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="60x60"
+            href="/icons/favicon/apple-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="72x72"
+            href="/icons/favicon/apple-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/icons/favicon/apple-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="114x114"
+            href="/icons/favicon/apple-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="120x120"
+            href="/icons/favicon/apple-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="144x144"
+            href="/icons/favicon/apple-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="152x152"
+            href="/icons/favicon/apple-icon-152x152.png"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/icons/favicon/apple-icon-180x180.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="192x192"
+            href="/icons/favicon/android-icon-192x192.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/icons/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="96x96"
+            href="/icons/favicon/favicon-96x96.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/icons/favicon/favicon-16x16.png"
+          />
+          <meta
+            name="msapplication-TileImage"
+            content="/icons/favicon/ms-icon-144x144.png"
+          ></meta>
           {/*<link*/}
           {/*  rel="preconnect"*/}
           {/*  href={process.env.NEXT_PUBLIC_BASE_URL}*/}
