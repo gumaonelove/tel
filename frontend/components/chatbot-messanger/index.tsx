c59ce7ce993aabc06f4c6805e4c259cb0ba599f3
Ildar <ildarvaliev167@gmail.com>
2023-08-10 17:03:08 +0500

    frontend changes

--- frontend/components/chatbot-messanger/index.tsx
+++ frontend/components/chatbot-messanger/index.tsx
@@ -4,46 +4,90 @@ import TextInput from "../common/text-input";
 import {useEffect, useState} from "react";
 import apiClient from "../../lib/axios";
 import {API_URL} from "../../lib/constants";
-import {IAllMessages} from "./types";
 import {toast} from "react-toastify";
+
+class Node {
+  message: string;
+  prev: Node | null;
+  next: Node | null;
+
+  constructor(message: string) {
+    this.message = message;
+    this.prev = null;
+    this.next = null;
+  }
+}
+
+class DoublyLinkedList {
+  head: Node | null;
+  tail: Node | null;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  addMessage(message: string) {
+    const newNode = new Node(message);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      if (this.tail) {
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+      }
+    }
+  }
+  getMessages() {
+    let current = this.head;
+    const messages: string[] = [];
+    while (current) {
+      messages.push(current.message);
+      current = current.next;
+    }
+    return messages;
+  }
+}
 
 const ChatbotMessanger = () => {
   const [showMessages, setShowMessages] = useState<boolean>(false);
   const [inputValue, setInputValue] = useState<string>("");
   const [blockedMessages, setBlockedMessages] = useState<boolean>(false);
-  const [allMessages, setAllMessages] = useState<string[]>(["Сәлам! Эшләрең ничек? Миңа берәр нәрсә яз."]);
+  const [allMessages, setAllMessages] = useState<DoublyLinkedList>(new DoublyLinkedList());
+  const [messages, setMessages] = useState<string[]>([]);
   const [isLoading, setLoading] = useState<boolean>(false);
-
   const sendMessage = async () => {
     setLoading(true);
-    let dataToServer = [...allMessages]
-    await apiClient(API_URL).post(
-      `/dialogue/`, dataToServer
-    ).then((response) => {
-      setAllMessages([
-        ...allMessages,
-        response.data.output
-      ]);
-      setBlockedMessages(false);
-      setLoading(false);
-    }).catch((err) => {
-      console.log(err)
-      toast.error("Произошла ошибка при генерации ответа, пожалуйтса, попробуйте снова!");
-      setLoading(false);
-    })
+    let dataToServer = allMessages.getMessages();
+    await apiClient(API_URL)
+      .post(`/dialogue/`, dataToServer)
+      .then((response) => {
+        const output = response.data.output;
+        allMessages.addMessage(output);
+        setAllMessages(allMessages);
+        setMessages(allMessages.getMessages());
+        setBlockedMessages(false);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err)
+        toast.error("Произошла ошибка при генерации ответа, пожалуйтса, попробуйте снова!");
+        setLoading(false);
+      })
   }
 
   const setNewMessages = (message: string) => {
     if (!showMessages) {
       setShowMessages(true);
     }
-    allMessages.push(message)
-    setAllMessages([
-      ...allMessages
-    ]);
+    allMessages.addMessage(message);
+    setAllMessages(allMessages);
+    setMessages(allMessages.getMessages());
     setInputValue("");
     setBlockedMessages(true);
-  }
+  };
   const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     if (!blockedMessages) {
@@ -59,6 +103,14 @@ const ChatbotMessanger = () => {
     }
   }
 
+  useEffect(() => {
+    allMessages.addMessage("Сәлам! Эшләрең ничек? Миңа берәр нәрсә яз.");
+  }, []);
+
+  useEffect(() => {
+    setMessages(allMessages.getMessages());
+  }, [allMessages])
+
   return (
     <section className="chatbot">
       <div className="chatbot__container container">
@@ -68,7 +120,7 @@ const ChatbotMessanger = () => {
               showMessages
               ?
                 <ChatbotMessages
-                  allMessages={allMessages}
+                  allMessages={messages}
                   isLoading={isLoading}
                 />
                 :
@@ -111,3 +163,5 @@ const ChatbotMessanger = () => {
 }
 
 export default ChatbotMessanger;
+
+
