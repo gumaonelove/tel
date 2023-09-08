634f83790d2c0251702c5a9e30e7041b7eb4c27d
Ildar <ildarvaliev167@gmail.com>
2023-09-08 13:31:51 +0300

    changed team

--- frontend/src/widgets/Team/const/links.ts
+++ frontend/src/widgets/Team/const/links.ts
@@ -13,3 +13,46 @@ export const IldarLinks: TeamLink[] = [
         link: 'https://github.com/Ildar7',
     },
 ];
+
+export const BulatLinks: TeamLink[] = [
+    {
+        name: 'Telegram',
+        link: 'https://t.me/gumerovbr',
+    },
+    {
+        name: 'GitHub',
+        link: 'https://github.com/gumaonelove',
+    },
+];
+
+export const KerilLinks: TeamLink[] = [
+    {
+        name: 'Telegram',
+        link: 'https://t.me/gigasterk',
+    },
+    {
+        name: 'GitHub',
+        link: 'https://github.com/gigaster-ops',
+    },
+];
+
+export const RALinks: TeamLink[] = [
+    {
+        name: 'Scientific page',
+        link: 'http://www.antat.ru/ru/ips/about/employees/gilmullin_ra.php',
+    },
+];
+
+export const GALinks: TeamLink[] = [
+    {
+        name: 'Scientific page',
+        link: 'http://www.antat.ru/ru/ips/about/employees/gatiatullin_ar.php',
+    },
+];
+
+export const HBLinks: TeamLink[] = [
+    {
+        name: 'Scientific page',
+        link: 'http://www.antat.ru/ru/ips/about/employees/khakimov_be.php',
+    },
+];
--- frontend/src/widgets/Team/ui/Team.tsx
+++ frontend/src/widgets/Team/ui/Team.tsx
@@ -6,28 +6,14 @@ import AyratImage from 'shared/assets/images/ar.jpg';
 import BulatImage from 'shared/assets/images/be.jpg';
 import BulychImage from 'shared/assets/images/bulych.jpg';
 import KerilImage from 'shared/assets/images/keril.jpg';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import cls from './Team.module.scss';
-import { IldarLinks } from '../const/links';
+import {
+    BulatLinks, GALinks, HBLinks, IldarLinks, KerilLinks, RALinks,
+} from '../const/links';
 
 interface TeamProps {
   className?: string;
 }
-
-const breakpoints = {
-    0: {
-        slidesPerView: 1,
-    },
-    560: {
-        slidesPerView: 2,
-    },
-    1200: {
-        slidesPerView: 3,
-    },
-    1550: {
-        slidesPerView: 4,
-    },
-};
 export const Team = (props: TeamProps) => {
     const {
         className,
@@ -35,72 +21,48 @@ export const Team = (props: TeamProps) => {
 
     return (
         <div className={classNames(cls.Team, {}, [className])}>
-            <Swiper
-                className={cls.swiper}
-                breakpoints={breakpoints}
-                spaceBetween={20}
-            >
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Гильмуллин Ринат"
-                        subtitle="Директор ИПС АН РТ"
-                        image={RinatImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Гатиатуллин Айрат"
-                        subtitle="Заместитель директора ИПС АН РТ"
-                        image={AyratImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Хакимов Булат"
-                        subtitle="Ведущий научный сотрудник ИПС АН РТ"
-                        image={BulatImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Валиев Ильдар"
-                        subtitle="Frontend"
-                        image={IldarImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Гумеров Булат"
-                        subtitle="Backend"
-                        image={BulychImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-                <SwiperSlide
-                    className={cls.slide}
-                >
-                    <TeamCard
-                        title="Пантелеев Кирилл"
-                        subtitle="ML — machine learning"
-                        image={KerilImage}
-                        links={IldarLinks}
-                    />
-                </SwiperSlide>
-            </Swiper>
+            <TeamCard
+                title="Гильмуллин Ринат"
+                subtitle="Директор ИПС АН РТ"
+                image={RinatImage}
+                className={cls.card}
+                links={RALinks}
+            />
+            <TeamCard
+                title="Гатиатуллин Айрат"
+                subtitle="Заместитель директора ИПС АН РТ"
+                image={AyratImage}
+                className={cls.card}
+                links={GALinks}
+            />
+            <TeamCard
+                title="Хакимов Булат"
+                subtitle="Ведущий научный сотрудник ИПС АН РТ"
+                image={BulatImage}
+                className={cls.card}
+                links={HBLinks}
+            />
+            <TeamCard
+                title="Валиев Ильдар"
+                subtitle="Frontend"
+                image={IldarImage}
+                links={IldarLinks}
+                className={cls.card}
+            />
+            <TeamCard
+                title="Гумеров Булат"
+                subtitle="Backend & machine learning"
+                image={BulychImage}
+                links={BulatLinks}
+                className={cls.card}
+            />
+            <TeamCard
+                title="Пантелеев Кирилл"
+                subtitle="Machine learning"
+                image={KerilImage}
+                links={KerilLinks}
+                className={cls.card}
+            />
         </div>
     );
 };
